fc0be89877c1fb1620656a90b942992a05349605
nishantka <nishantkaushikadobe@gmail.com>
2025-05-05 13:57:47 +0530

    MWPW-168461 - Add aria label

--- libs/blocks/global-navigation/utilities/menu/menu.js
+++ libs/blocks/global-navigation/utilities/menu/menu.js
@@ -185,6 +185,10 @@ const decoratePromo = (elem, index) => {
     decorateImage();
   }
 
+  const description = elem.querySelector('.feds-promo-content p')?.textContent;
+  const actualText = elem.querySelector('.feds-promo-content a').textContent;
+  elem.querySelector('.feds-promo-content a')?.setAttribute('aria-label', `${description} ${actualText}`);
+
   elem.classList = 'feds-promo';
 
   if (isDarkTheme) {
